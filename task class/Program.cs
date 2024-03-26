7dfc1a3d4dedf364a69c5efa7608f58ff2e4aa77
Kenanpasayev <tu7kr6n7r@code.edu.az>
2024-03-26 10:38:19 +0400

    task class

---
+++ task class/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.ComponentModel.Design;
+using Task_class;
+
+namespace Task_class
+{
+    //Task-2
+    //public class Product
+    //{
+    //    public string Name;
+    //    public string Description;
+    //    public int Count;
+    //    public bool IsStock;
+    //    public double Price;
+    //    public Product(string name,string description,int count,bool isstock,double price)
+    //    {
+    //        Name = name;
+    //        Description = description;
+    //        Count = count;
+    //        IsStock = isstock;
+    //        Price = price;
+    //    }
+    //    public void Sale() 
+    //    {
+    //        if (Count > 0)
+    //        {
+    //            Count--;
+    //            Console.WriteLine($"Name:{Name},Description:{Description},Count:{Count},IsStock:{IsStock},Price:{Price},");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Mehsul yoxdur!");
+    //        }
+    //    }
+     
+    //}
+    //public class Notebook:Product
+
+    //{
+    //    public byte RAM;
+    //    public int Storage;
+    //    public Notebook(string name,string description,int count,bool isstock,double price,byte RAM,int Storage) : base (name, description, count, isstock, price)
+    //    {
+    //        this.RAM = RAM;
+    //        this.Storage = Storage;
+
+    //    }
+    //    public void GetFullInfo()
+    //    {
+    //        Console.WriteLine($"Name:{Name},Description:{Description},Count:{Count},IsStock:{IsStock},Price:{Price},RAM:{RAM},Storage:{Storage}");
+    //    }
+    //    public void ShowFullData()
+    //    {
+    //        if (Count > 0)
+    //        {
+    //            GetFullInfo();
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Mehsul yoxdur!");
+    //        }
+    //    }
+      
+    }
+       
+   
+        
+    
+
+
+
+    //Task -1
+    //public class Vehicle
+    //{
+    //    public string Brand;
+    //    public string Model;
+    //    public double Mile;
+    //    public Vehicle(string brand, string model, double mile)
+    //    {
+    //        Brand = brand;
+    //        Model = model;
+    //        Mile = mile;
+    //    }
+    //    public void ShowFullInfo()
+    //    {
+    //        Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile}");
+    //    }
+    //}
+
+    //public class Car : Vehicle
+    //{
+    //    public int DoorsNum;
+    //    public Car(string brand, string model, double mile, int doorsNum) : base(brand, model, mile)
+    //    {
+    //        DoorsNum = doorsNum;
+    //    }
+    //    public void ShowFullInfo()
+    //    {
+    //        Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile},DoorsNum:{DoorsNum}");
+    //    }
+    //}
+
+    //public class MotorCycle : Vehicle
+    //{
+    //    public int WheelNum;
+    //    public MotorCycle(string brand, string model, double mile, int wheelNum) : base(brand, model, mile)
+    //    {
+    //        WheelNum = wheelNum;
+    //    }
+    //    public void ShowFullInfo()
+    //    {
+    //        Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile},WheelNum:{WheelNum}");
+    //    }
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+        //Task-2
+        //Notebook mynotebook = new Notebook("Lenova","Masaustu", 7,true, 2000, 16, 512);
+        //mynotebook.Sale();
+        //mynotebook.ShowFullData();
+        
+        //Task-1
+        //Car mycar = new Car("Toyota", "Prius", 70000, 4);
+        //mycar.ShowFullInfo();
+
+        //MotorCycle mymotorCycle = new MotorCycle("Ferrari", "E-5", 22000, 2);
+        //mymotorCycle.ShowFullInfo();
+        }
+    }
+
+   
